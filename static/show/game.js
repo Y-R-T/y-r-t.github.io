1d8033e146bb3e80cbd4407846013f937072e8b2
ruotian <ruotian2021@gmail.com>
2024-05-24 11:49:18 +0800

    Site updated:YYYY-05-2024年5月24日 11:49:18

--- static/show/game.js
+++ static/show/game.js
@@ -33,39 +33,56 @@ class Key{
     }
 }
 
-var keyBoard1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"];
-var keyBoardCode1 = ["KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash"];
+var keyBoard1 = ["Tab","q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"];
+var keyBoardCode1 = ["Tab","KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash"];
 var keyList1 = [];
 
-var keyBoard2 = ["a","s","d","f","g","h","j","k","l",";","\'","Enter"];
-var keyBoardCode2 = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL","Semicolon","Quote","Enter"];
+var keyBoard2 = ["CL","a","s","d","f","g","h","j","k","l",";","\'","Enter"];
+var keyBoardCode2 = ["CapsLock","KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL","Semicolon","Quote","Enter"];
 var keyList2 = [];
 
 var keyBoard3 = ["Shift","z","x","c","v","b","n","m",",",".","/","Shift"];
 var keyBoardCode3 = ["ShiftLeft","KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period","Slash","ShiftRight"];
 var keyList3 = [];
+
+var keyBoard4 = ["Space"];
+var keyBoardCode4 = ["Space"];
+var keyList4 = [];
 for (let index = 0; index < keyBoard1.length; index++) {
+    if (index == 0) {
+        keyList1.push(new Key(keyBoard1[index], (index)*60 + 10, 100, 150));
+        continue;
+    }
     keyList1.push(new Key(keyBoard1[index], (index+1)*60 + 50, 100, 50));
 }
 
 for (let index = 0; index < keyBoard2.length; index++) {
+    if (index == 0) {
+        keyList2.push(new Key(keyBoard2[index], (index)*60 + 10, 170, 160));
+        continue;
+    }
     if (index == keyBoard2.length - 1) {
-        keyList2.push(new Key(keyBoard2[index], (index+1)*60 + 70, 170, 150));
+        keyList2.push(new Key(keyBoard2[index], (index)*60 + 120, 170, 150));
         break;
     }
-    keyList2.push(new Key(keyBoard2[index], (index+1)*60 + 70, 170, 50));
+    keyList2.push(new Key(keyBoard2[index], (index)*60 + 120, 170, 50));
 }
 
 for (let index = 0; index < keyBoard3.length; index++) {
     if (index == 0) {
-        keyList3.push(new Key(keyBoard3[index], (index)*60 - 10, 240, 150));
+        keyList3.push(new Key(keyBoard3[index], (index)*60 + 10, 240, 180));
         continue;
     }
     if (index == keyBoard3.length - 1) {
-        keyList3.push(new Key(keyBoard3[index], (index)*60 + 90, 240, 150));
+        keyList3.push(new Key(keyBoard3[index], (index)*60 + 140, 240, 190));
         break;
     }
-    keyList3.push(new Key(keyBoard3[index], (index)*60 + 90, 240, 50));
+    keyList3.push(new Key(keyBoard3[index], (index)*60 + 140, 240, 50));
+}
+
+for (let index = 0; index < keyBoard4.length; index++) {
+//    keyList4.push(new Key(keyBoard4[index], (index+1)*60 + 50, 100, 50));
+    keyList4.push(new Key(keyBoard4[index], (index)*60 + 320, 310, 300));
 }
 
 context.font = '50px Arial';
@@ -88,6 +105,11 @@ document.addEventListener('keydown', function (event) {
             keyList3[index].down();
         }
     }
+    for (let index = 0; index < keyBoard4.length; index++) {
+        if (event.code === keyBoardCode4[index]) {
+            keyList4[index].down();
+        }
+    }
 });
 
 document.addEventListener('keyup', function (event) {
@@ -106,6 +128,11 @@ document.addEventListener('keyup', function (event) {
             keyList3[index].up();
         }
     }
+    for (let index = 0; index < keyBoard4.length; index++) {
+        if (event.code === keyBoardCode4[index]) {
+            keyList4[index].up();
+        }
+    }
 });
 
 function update() {
@@ -117,6 +144,9 @@ function update() {
     }
     for (let index = 0; index < keyList3.length; index++) {
         keyList3[index].draw();   
+    }
+    for (let index = 0; index < keyList4.length; index++) {
+        keyList4[index].draw();   
     }
     
 }
